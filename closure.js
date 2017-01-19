607f676e9dbc87b92057d2f1bfe698ca0d672cda
rashmi-95 <rashmi05rash@gmail.com>
2017-01-19 23:59:59 +0530

    -A

--- closure.js
+++ closure.js
@@ -6,3 +6,19 @@ function foo() {
         var quux = 10;
     }
 }
+/*
+function makeAdder(x) {
+  return function(y) {
+      x++;
+      console.log('ghy',x);
+    return x + y;
+  };
+}
+
+var add5 = makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2));  // 
+console.log(add5(3));  //
+console.log(add10(2)); // 12
+*/
